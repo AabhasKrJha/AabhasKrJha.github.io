a002723428512789840978845dd54ef728ec604f
AabhasKrJha <abhasjha03@gmail.com>
2021-08-30 20:21:28 +0530

    theme update

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@
 function change_theme(theme){
 
     var theme_css_sheet = document.getElementById("theme-sheet");
+    var theme_icon = document.getElementById(theme);
     const light_theme = "css/default/default_light_theme.css";
     const dark_theme = "css/dark_themes/default_dark/default_dark.css";
     const dim_theme = "css/dark_themes/dim_dark/dim_dark.css";
     const dark_red_theme = "css/dark_themes/red_dark/red_dark.css";
+    
 
     if (theme === 'light'){
         theme_css_sheet.href = light_theme;
@@ -16,6 +18,8 @@ function change_theme(theme){
     } else if (theme === 'red'){
         theme_css_sheet.href = dark_red_theme;
     };
+
+    theme_icon.style.border = '4px dotted green'
 
 };
 
